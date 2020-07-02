ca33763095c5f538abbcbee1492efdde3cfb69c1
Satyajit Ghana <satyajitghana7@gmail.com>
2020-07-02 23:52:08 +0530

    Update setup_ubuntu_machine.sh

--- setup_ubuntu_machine.sh
+++ setup_ubuntu_machine.sh
@@ -21,3 +21,8 @@ curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
 sudo apt-get update && sudo apt-get install spotify-client
+
+# VLC
+sudo add-apt-repository ppa:videolan/master-daily
+sudo apt-get update
+sudo apt-get install vlc
