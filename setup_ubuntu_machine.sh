5f41071c251a7676bb9c444baed65d54faa3e77f
Satyajit Ghana <satyajitghana7@gmail.com>
2020-07-02 23:10:21 +0530

    Update setup_ubuntu_machine.sh

--- setup_ubuntu_machine.sh
+++ setup_ubuntu_machine.sh
@@ -3,7 +3,7 @@
 # INSTALL REGOLITH
 sudo add-apt-repository ppa:regolith-linux/release
 sudo apt install regolith-desktop
-sudo apt install i3xrocks-net-traffic i3xrocks-cpu-usage i3xrocks-time
+sudo apt install i3xrocks-net-traffic i3xrocks-cpu-usage i3xrocks-time i3xrocks-memory i3xrocks-weather
 sudo apt install i3xrocks-battery # for laptops
 
 # TILIX
