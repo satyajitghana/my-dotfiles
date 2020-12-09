b6dfc785eacc0740445d7651cdd1180028afa741
Satyajit Ghana <satyajitghana7@gmail.com>
2020-12-09 19:20:00 +0530

    Update setup_jupyter.sh

--- setup_jupyter.sh
+++ setup_jupyter.sh
@@ -1,6 +1,7 @@
 # Install jupyter notebook, lab, nbblack and themes
 conda install -c conda-forge notebook jupyterlab jupyterthemes nb_black
 conda install -c fastai nbdev
+pip install nbconvert==5.6.1 # since there's issue with python 3.8/3.9 installed nbconvert version
 jt -t monokai -f fira -fs 13 -nf ptsans -nfs 11 -N -kl -cursw 5 -cursc r -cellw 95% -T
 
 # Jupyter Lab Themes
