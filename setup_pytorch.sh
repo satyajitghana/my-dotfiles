c162ade1cdf82709a934e6f683e7466d8126ff82
Satyajit Ghana <satyajitghana7@gmail.com>
2020-12-13 21:11:12 +0530

    Update setup_pytorch.sh

--- setup_pytorch.sh
+++ setup_pytorch.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# install cpu version of pytorch
 conda install pytorch torchvision torchaudio cpuonly -c pytorch
+
+# install albumentations
+conda install -c conda-forge imgaug
+conda install -c conda-forge albumentations
