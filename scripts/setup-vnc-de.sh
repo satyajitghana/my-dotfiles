548cb5130f4bd1794660138b727def9dccea3a2b
Satyajit Ghana <satyajitghana7@gmail.com>
2021-11-27 12:03:38 +0000

    add vnc setup

--- scripts/setup-vnc-de.sh
+++ scripts/setup-vnc-de.sh
@@ -20,3 +20,15 @@ vncserver
 echo -e "use below to port forward 5901 to localhost"
 echo -e "ssh -L 5901:127.0.0.1:5901 -C -N -l $USER $HOSTNAME"
 echo -e "now connect to localhost:5901 using VNC Viewer"
+
+# add as service
+
+sed -e "s;%USER%;$USER;g" vncserver@.service > vncserver@.service
+
+cp vncserver@.service /etc/systemd/system/
+
+sudo systemctl daemon-reload
+
+# open on 5901
+# if you want to killuse: `vncserver -kill :1``
+sudo systemctl enable vncserver@1.service
