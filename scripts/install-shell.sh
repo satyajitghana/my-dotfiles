cd59a93fce06bee38ae419822e8c58c9dfac3887
satyajit-ink <88043650+satyajit-ink@users.noreply.github.com>
2022-02-03 14:24:01 +0530

    Update install-shell.sh

--- scripts/install-shell.sh
+++ scripts/install-shell.sh
@@ -8,7 +8,7 @@ NC='\033[0m'
 sudo apt install zsh -y
 echo "${RED}installed zsh${NC}"
 
-sudo apt install exa bat thefuck
+sudo apt install exa bat thefuck fzf
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
